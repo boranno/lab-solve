9ae87d2c4004612fa84546b7df98c9b01a9318d0
Boranno Golder <48504390+boranno@users.noreply.github.com>
2022-09-26 13:01:01 +0600

    Add files via upload

--- 3.c
+++ 3.c
@@ -1,50 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int m;
-    scanf("%d",&m);
-    switch(m)
+    int size;
+    printf("Enter total Student\n");
+    scanf("%d",&size);
+    int arr[size+1],avg=0,c=0;
+    printf("Enter The Marks\n");
+    for(int i=0;i<=size;i++)
     {
-    case 1:
-        printf("%d\n",31);
-        break;
-    case 2:
-        printf("%d\n",28);
-        break;
-    case 3:
-        printf("%d\n",31);
-        break;
-    case 4:
-        printf("%d\n",30);
-        break;
-    case 5:
-        printf("%d\n",31);
-        break;
-    case 6:
-        printf("%d\n",30);
-        break;
-    case 7:
-        printf("%d\n",31);
-        break;
-    case 8:
-        printf("%d\n",31);
-        break;
-    case 9:
-        printf("%d\n",30);
-        break;
-    case 10:
-        printf("%d\n",31);
-        break;
-    case 11:
-        printf("%d\n",30);
-        break;
-    case 12:
-        printf("%d\n",31);
-        break;
-    default:
-        printf("invalid\n");
+        scanf("%d",&arr[i]);
+        c++;
     }
+    for(int i=0;i<=size;i++)
+    {
+        avg+=arr[i];
+    }
+    printf("Avg = %d\n",avg/c);
+
     return 0;
 }
-
 
